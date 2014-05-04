d56e2e59f9cec25af20dc3ff31e05e486a5c7ebf
jturco <jturco@esri.com>
2014-05-04 19:31:31 -0400

    Added to JS\main.js page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * @author Justin
+ */
+require([
+	"dojo/parser",
+    
+    "dijit/layout/BorderContainer","dijit/layout/TabContainer", "dijit/layout/ContentPane", "dojo/domReady!"
+    ], function(parser){
+    	parser.parse();	
+});
